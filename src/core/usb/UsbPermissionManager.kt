16b4cccfbd1fdc7db5b881bf90ffdae32711dd24
رأفت عمر <shamwebs1@gmail.com>
2026-05-04 11:43:11 +0300

    Create UsbPermissionManager.kt

---
+++ src/core/usb/UsbPermissionManager.kt
@@ -0,0 +1,15 @@
+// File: src/core/usb/UsbPermissionManager.kt
+package core.usb
+
+class UsbPermissionManager {
+
+    fun hasPermission(): Boolean {
+        // لاحقاً: فحص صلاحيات USB الحقيقية
+        return true
+    }
+
+    fun requestPermission(): Boolean {
+        // لاحقاً: طلب صلاحيات USB من النظام
+        return true
+    }
+}
