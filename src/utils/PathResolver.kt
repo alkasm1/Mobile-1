e77ddda59b0bb960731fd2eff7a543eb4062b8cb
رأفت عمر <shamwebs1@gmail.com>
2026-05-04 11:45:18 +0300

    Create PathResolver.kt

---
+++ src/utils/PathResolver.kt
@@ -0,0 +1,11 @@
+// File: src/utils/PathResolver.kt
+package utils
+
+object PathResolver {
+
+    fun bootImg(): String = "assets/firmware/boot.img"
+
+    fun systemImg(): String = "assets/firmware/system.img"
+
+    fun vbmetaImg(): String = "assets/firmware/vbmeta.img"
+}
